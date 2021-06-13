ae2ea9018ba86b3fd369a6376c149346567e365c
amorales <amorales@iuma.ulpgc.es>
2021-06-13 13:55:11 +0100

    update django settings

--- estore_django/estore_django/settings.py
+++ estore_django/estore_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!s91e+3(n3k_rt62+sw2uigq$9&ol#23p4ji6epl_o@uosnl_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
